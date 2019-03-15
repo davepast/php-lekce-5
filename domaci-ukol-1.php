1620134a813b094b7782747fd20bcb91355ba4dc
davepast <david.pastuszek@hotmail.com>
2019-03-15 12:12:35 +0100

    domaci ukol úprava vypsání jmen v dú2 atd

--- domaci-ukol-1.php
+++ domaci-ukol-1.php
@@ -11,95 +11,75 @@ function vypisDetaily(TrojrozmernyObrazec $teleso) {
 }
 
 
-class Kvadr implements TrojrozmernyObrazec
-{
+class Kvadr implements TrojrozmernyObrazec {
     public $kvadrStranaA;
     public $kvadrStranaB;
     public $kvadrStranaC;
 
-    public function ziskejObjem()
-    {
+    public function ziskejObjem() {
         return $this->kvadrStranaA * $this->kvadrStranaB * $this->kvadrStranaC;
     }
-    public function ziskejPovrch()
-    {
+
+    public function ziskejPovrch() {
         return 2 * ($this->kvadrStranaA * $this->kvadrStranaB + $this->kvadrStranaA * $this->kvadrStranaC + $this->kvadrStranaA * $this->kvadrStranaC);
     }
 
-    public function __construct($kvadrStranaA, $kvadrStranaB, $kvadrStranaC)
-    {
+    public function __construct($kvadrStranaA, $kvadrStranaB, $kvadrStranaC) {
         $this->kvadrStranaA = $kvadrStranaA;
         $this->kvadrStranaB = $kvadrStranaB;
         $this->kvadrStranaC = $kvadrStranaC;
     }
 }
 
-class Krychle implements TrojrozmernyObrazec
-{
+class Krychle implements TrojrozmernyObrazec {
     public $stranaKrychle;
 
-    public function __construct($stranaA)
-    {
+    public function __construct($stranaA) {
         $this->stranaKrychle = $stranaA;
     }
 
-    public function ziskejObjem()
-    {
+    public function ziskejObjem() {
         return pow($this->stranaKrychle, 3);
     }
 
-    public function ziskejPovrch()
-    {
+    public function ziskejPovrch() {
         return 6 * pow($this->stranaKrychle, 2);
     }
 }
 
-class Koule implements TrojrozmernyObrazec
-{
-
+class Koule implements TrojrozmernyObrazec {
     public $polomer;
 
-    public function __construct($polomer)
-    {
+    public function __construct($polomer) {
         $this->polomer = $polomer;
     }
 
-    public function ziskejPovrch()
-    {
+    public function ziskejPovrch() {
         return 4 * M_PI * pow($this->polomer, 2);
     }
 
-    public function ziskejObjem()
-    {
+    public function ziskejObjem() {
         return 4 * M_PI * pow($this->polomer, 3) / 3;
     }
 }
 
-class Jehlan implements TrojrozmernyObrazec
-{
+class Jehlan implements TrojrozmernyObrazec {
     public $jehlanStrana;
     public $jehlanVyskaNaStranu;
 
-    public function ziskejObjem()
-    {
+    public function ziskejObjem() {
         return pow($this->jehlanStrana, 2) * $this->jehlanVyskaNaStranu / 3;
     }
 
-    public function ziskejPovrch()
-    {
+    public function ziskejPovrch() {
         return (pow($this->jehlanStrana, 2) + 4 * $this->jehlanStrana * (sqrt(pow($this->jehlanStrana /2 , 2) + pow($this->jehlanVyskaNaStranu,2))));
-
-
     }
 
-    public function __construct($jehlanStrana, $jehlanVyskaNaStranu)
-    {
+    public function __construct($jehlanStrana, $jehlanVyskaNaStranu) {
         $this->jehlanStrana = $jehlanStrana;
         $this->jehlanVyskaNaStranu = $jehlanVyskaNaStranu;
     }
 }
-
-
 
 $kvadr1 = new Kvadr(2,3,7);
 $krychle1 = new Krychle(5);
--- domaci-ukol-2.php
+++ domaci-ukol-2.php
@@ -1,10 +1,8 @@
 <?php
 
-class Uzivatel
-{
-    public $jmeno;
-
-    public $heslo;
+class Uzivatel {
+    private $jmeno;
+    private $heslo;
 
     public function __construct(string $jmeno, string $heslo)
     {
@@ -12,8 +10,7 @@ class Uzivatel
         $this->heslo = $heslo;
     }
 
-    public function overeni(string $zadaneJmeno, string $zadaneHeslo)
-    {
+    public function overeni(string $zadaneJmeno, string $zadaneHeslo) {
         if ($this->jmeno == $zadaneJmeno && $this->heslo == $zadaneHeslo) {
             return true;
         } else {
@@ -21,58 +18,49 @@ class Uzivatel
         }
     }
 
-    public function ziskejJmeno()
-    {
+    public function ziskejJmeno() {
         return $this->jmeno;
     }
 }
 
-class Prihlasovani
-{
-    public $prihlaseniUzivatele;
+class Prihlasovani {
+    private $prihlaseniUzivatele;
 
-    public function prihlas(Uzivatel $uzivatel, string $prihlasitJmeno, string $prihlasitHeslo)
-    {
+    public function prihlas(Uzivatel $uzivatel, string $prihlasitJmeno, string $prihlasitHeslo) {
         if ($uzivatel->overeni($prihlasitJmeno, $prihlasitHeslo) === true) {
             $this->prihlaseniUzivatele[] = $uzivatel->ziskejJmeno();
         }
-
     }
 
-    public function zobrazPrihlaseneUzivatele(){
-
-    echo "Přihlášení uživatelé: ";
-    if(empty($this->prihlaseniUzivatele)) {
-
-    } else {
-        foreach ($this->prihlaseniUzivatele as $value) {
-        echo $value . ", ";
+    public function zobrazPrihlaseneUzivatele() {
+        echo "Přihlášení uživatelé: ";
+        if(empty($this->prihlaseniUzivatele)) {
+        echo "nikdo";
+        } else {
+            foreach ($this->prihlaseniUzivatele as $key => $value) {
+                if ($key == count($this->prihlaseniUzivatele) - 1) {
+                    echo $value;
+                } else {
+                    echo $value . ", ";
+                }
+            }
         }
+        echo "<br>";
     }
-    echo "<br>";
-
-    }
-
 }
 
 $josef = new Uzivatel('josef', 'josef1234');
-
 $prihlasovani = new Prihlasovani();
-
 $prihlasovani->zobrazPrihlaseneUzivatele();
 
 $prihlasovani->prihlas($josef, 'josef', 'josef1234');
-
 $prihlasovani->zobrazPrihlaseneUzivatele();
 
 $karel = new Uzivatel('karel', 'karel1234');
-
 $prihlasovani->prihlas($karel, 'karel', 'kar34');
-
 $prihlasovani->zobrazPrihlaseneUzivatele();
 
 $prihlasovani->prihlas($karel, 'karel', 'karel1234');
-
 $prihlasovani->zobrazPrihlaseneUzivatele();
 
 
